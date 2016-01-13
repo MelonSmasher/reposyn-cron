aa1e43c59ccc3d64d49ec27f9e448ebe201cb6bb
Alex Markessinis <markea@almyz125.com>
2016-01-13 14:10:24 -0500

    logging reposync output

--- reposync.cron.sh
+++ reposync.cron.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-sudo -u reposync /usr/local/sbin/repo_sync
+sudo -u reposync /usr/local/sbin/repo_sync > /var/log/reposync.log
 /bin/chown -R reposync:nginx /mnt/swus/Data/
 /bin/chmod -R 755 /mnt/swus/Data/
